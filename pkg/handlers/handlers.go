ee94268788a18e3557f17b15b916b09e21a11bdc
sangnh <sangnh@futurify.vn>
2021-07-12 23:48:14 +0700

    Converting pages to Go templates

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -46,3 +46,15 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 		StringMap: stringMap,
 	})
 }
+
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) Availablity(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-availblity.page.tmpl", &models.TemplateData{})
+}
